906b558a73f8bc270947d34244538f70eb7aa13f
chpeih <peihuanchen01@gmail.com>
2017-02-16 09:17:51 +0800

    http简单请求回复

--- server01.cc
+++ server01.cc
@@ -9,10 +9,17 @@
 using namespace std;
 int HTTP_SERVER_PORT = 9008;
 int MAX_QUEUE_SIZE = 1000;
+int BUFF_SIZE = 1000;
 
-int main()
+int main( int argc, char *argv[])
 {
 
+	if( argc != 2 )
+	{
+		printf("usage: %s port\n", argv[0]);
+		return 1;
+	}
+	int port = atoi(argv[1]);
 	int listenfd, connfd;
 	pid_t childpid;
 	socklen_t clilen;
@@ -26,7 +33,7 @@ int main()
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(HTTP_SERVER_PORT);
+	servaddr.sin_port = htons(port);
 
 	if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
 	{
@@ -40,9 +47,9 @@ int main()
 		exit(-1);
 	}
 
-	printf("http server start on port %d\n", HTTP_SERVER_PORT );
+	printf("http server start on port %d\n", port);
 
-
+	int count = 0;
 	for(;;)
 	{
 		clilen = sizeof(cliaddr);
@@ -52,11 +59,31 @@ int main()
 			perror("accept error");
 			exit(-1);
 		}
+
+		printf("accept a client %d\n", count);
+		count++;
+
+
 		if( (childpid = fork()) == 0 )
 		{
 			close(listenfd);
-			printf("do sth on child pthread");
-			exit(0);
+			char recv_buff[BUFF_SIZE];
+			int recv_len = recv(connfd, recv_buff, BUFF_SIZE, 0);
+			printf("reveive %d\n", recv_len);
+			printf("buff:%s\n",recv_buff); 
+			char send_buff[BUFF_SIZE];
+			sprintf(send_buff, "HTTP/1.1 200 OK\r\n");
+			send(connfd, send_buff, strlen(send_buff), 0);
+			sprintf( send_buff, "Server: hoohackhttpd/0.1.0\r\n");
+			send(connfd, send_buff, strlen(send_buff), 0);
+			sprintf( send_buff, "Content-Type: text/html\r\n");
+			send(connfd, send_buff, strlen(send_buff), 0);
+			strcpy(send_buff, "\r\n");
+			send(connfd, send_buff, strlen(send_buff), 0);
+			sprintf(send_buff, "Hello, this is from sysu http server\r\n");
+			send(connfd, send_buff, strlen(send_buff), 0);
+			close(connfd);
+			exit(1);
 		}
 		close(connfd);
 
